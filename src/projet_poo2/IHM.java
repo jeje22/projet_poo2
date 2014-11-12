40c42db9518737697be1dffe9cfcb304c507857c
Jérôme <jeromeboeglin@gmail.com>
2014-11-12 10:50:01 +0100

    ouverture fichier

--- src/projet_poo2/IHM.java
+++ src/projet_poo2/IHM.java
@@ -1,7 +1,26 @@
 package projet_poo2;
 
+import java.awt.FileDialog;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class IHM extends JFrame {
-
+public class IHM extends JFrame
+{
+	
+	public IHM()
+	{
+		super();
+		JFileChooser chooser = new JFileChooser();
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	        "JPG & PNG & GIF Images", "jpg", "png","gif","jpeg");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(this);
+	    if(returnVal == JFileChooser.APPROVE_OPTION)
+	    {
+	       System.out.println("You chose to open this file: " +
+	            chooser.getSelectedFile().getName());
+	    }
+	}
 }
