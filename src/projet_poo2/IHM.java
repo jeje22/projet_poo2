f398724030bad07611180f5d5d4be9ee49238aa5
LeoGaudias <leogaudias.pro@outlook.com>
2014-11-13 09:35:27 +0100

    option du crtl+O ajoutée

--- src/projet_poo2/IHM.java
+++ src/projet_poo2/IHM.java
@@ -3,6 +3,8 @@ package projet_poo2;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -13,7 +15,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class IHM extends JFrame 
+public class IHM extends JFrame implements KeyListener
 {
 	private static final long serialVersionUID = 1L;
 	private JMenuBar menuBar = new JMenuBar();
@@ -26,28 +28,14 @@ public class IHM extends JFrame
 		
 		this.setTitle("Project");
 		this.setPreferredSize(new Dimension(200,200));
+		this.addKeyListener(this);
 		this.menuBar.add(menu1);
 		this.menu1.add(item1);
 		item1.addActionListener(new ActionListener(){
 		      
 			public void actionPerformed(ActionEvent event)
 			{
-				JFileChooser chooser = new JFileChooser();
-			    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-			        "JPG & PNG & GIF Images", "jpg", "png","gif","jpeg");
-			    chooser.setFileFilter(filter);
-			    int returnVal = chooser.showOpenDialog(IHM.this);
-			    if(returnVal == JFileChooser.APPROVE_OPTION)
-			    {
-			       System.out.println("You chose to open this file: " +
-			            chooser.getSelectedFile().getName());
-			    }
-			   
-			   if(chooser.getSelectedFile() != null)
-			   {
-				   ouvrirImage(chooser.getSelectedFile().getAbsolutePath());
-			   }
-			    
+				openFileLocation();
 		    }
 		});
 		this.setJMenuBar(menuBar);
@@ -58,38 +46,39 @@ public class IHM extends JFrame
 		Picture pic=new Picture(filename);
 		ImageIcon ic = new ImageIcon(pic.image);
 		JLabel jl=new JLabel(ic);
-		//this.setContentPane(jl);
 		this.setSize(new Dimension(pic.width,pic.height));
 		this.add(jl);
 		
 	}
-}
-
-
-/*
-package projet_poo2;
-
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-
-public class IHM extends JFrame {
 	
-	private JMenuBar menuBar = new JMenuBar();
-	private JMenu menu1 = new JMenu("Fichier");
-	private JMenuItem item1 = new JMenuItem("Ouvrir");
-	
-	public IHM(){
-		super();
-		this.setTitle("Project");
-		this.setSize(1000,500);
-		this.menuBar.add(menu1);
-		this.menu1.add(item1);
-		this.setJMenuBar(menuBar);
+	public void openFileLocation(){
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		    "JPG & PNG & GIF Images", "jpg", "png","gif","jpeg");
+		chooser.setFileFilter(filter);
+		chooser.showOpenDialog(IHM.this);
+		if(chooser.getSelectedFile() != null)
+		{
+			ouvrirImage(chooser.getSelectedFile().getAbsolutePath());
+		}
 	}
-	
-	private static final long serialVersionUID = 1L;
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.isControlDown() && e.getKeyChar() != 'o' && e.getKeyCode() == 79) {
+			openFileLocation();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
-*/
